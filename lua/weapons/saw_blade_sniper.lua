db8cd6afd5827d2e522f1beb764903742aa62357
lordofthebombs <25441886+lordofthebombs@users.noreply.github.com>
2021-01-27 22:53:38 -0700

    Scope zooming works

--- lua/weapons/saw_blade_sniper.lua
+++ lua/weapons/saw_blade_sniper.lua
@@ -8,7 +8,7 @@ SWEP.IconOverride = ""
 SWEP.Spawnable = true
 SWEP.AdminOnly = false
 SWEP.BounceWeaponIcon = false
-if CLIENT then SWEP.WepSelectIcon = "" end
+-- if CLIENT then SWEP.WepSelectIcon = "" end
 
 
 -- Ammo info
@@ -24,6 +24,7 @@ SWEP.DrawCrosshair = true
 -- Setting secondary fire to do absolutely nothing
 SWEP.Secondary.ClipSize = 0
 SWEP.Secondary.DefaultClip = 0
+SWEP.Secondary.Automatic = false
 
 -- Weapon Switching info
 SWEP.Weight = 5
@@ -39,7 +40,22 @@ SWEP.ViewModel = "models/weapons/c_crossbow.mdl"
 SWEP.WorldModel = "models/weapons/w_crossbow.mdl"
 SWEP.UseHands = true
 
+-- scope variables
+SWEP.ZoomFOV = 20
+SWEP.Scoped = false
+SWEP.ZoomTransition = 0.15
+
 -- This will activate and deactivate the scope
 function SWEP:SecondaryAttack()
 
+    local owner = self:GetOwner()
+    print(owner)
+
+    if not scope_zoom then
+        owner:SetFOV(self.ZoomFOV, self.ZoomTransition)
+        scope_zoom = true
+    else
+        owner:SetFOV(0, self.ZoomTransition)
+        scope_zoom = false
+    end
 end
